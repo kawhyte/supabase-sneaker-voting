6efeaa185cafd7dc7b6b4fa14980e448d977240b
Kenny Whyte <kawhyte@gmail.com>
2024-03-12 18:20:48 -0700

    add to collection button added

--- components/SmoothieCard.js
+++ components/SmoothieCard.js
@@ -28,10 +28,27 @@ const SmoothieCard = ({ smoothie: sneaker, onDelete, onVote }) => {
 			onDelete(sneaker.id);
 		}
 	};
+	const handleAddToCollection = async (value,e) => {
+
+		console.log(" handleAddToCollection value", value);
+
+		const { data, error } = await supabase
+			.from("sneakers")
+			.update({in_collection:value})
+			.eq("id", sneaker.id)
+			.select();
+
+		if (error) {
+			console.log(error);
+		}
+		if (data) {
+			onDelete(sneaker.id);
+		}
+	};
 
 	const handleRating = async (value, e) => {
 		//setVote(value);
-		console.log("value", value);
+	
 
 		const { data, error } = await supabase
 			.from("sneakers")
@@ -187,6 +204,23 @@ const SmoothieCard = ({ smoothie: sneaker, onDelete, onVote }) => {
 							</svg>
 						</Link>
 
+						<svg
+
+						onClick={(e) => {
+							handleAddToCollection("true",e);
+						}}
+							
+							xmlns='http://www.w3.org/2000/svg'
+							viewBox='0 0 24 24'
+							fill='currentColor'
+							
+							className='w-6 h-6 hover:fill-red-400'>
+							<path
+								fillRule='evenodd'
+								d='M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25Zm-1.72 6.97a.75.75 0 1 0-1.06 1.06L10.94 12l-1.72 1.72a.75.75 0 1 0 1.06 1.06L12 13.06l1.72 1.72a.75.75 0 1 0 1.06-1.06L13.06 12l1.72-1.72a.75.75 0 1 0-1.06-1.06L12 10.94l-1.72-1.72Z'
+								clipRule='evenodd'
+							/>
+							</svg>
 						{/*<svg
 							onClick={handleDelete}
 							xmlns='http://www.w3.org/2000/svg'
