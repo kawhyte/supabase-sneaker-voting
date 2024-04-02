e7b83b3d218887f518713313a73276d5cfad6581
Kenny Whyte <kawhyte@gmail.com>
2024-04-02 14:01:11 -0700

    .

--- components/SectionHeader.tsx
+++ components/SectionHeader.tsx
@@ -4,7 +4,7 @@ import SupabaseLogo from "./SupabaseLogo";
 
 export default function Header({
 	name,
-	total=-1,
+	total = -1,
 	sectiontext,
 }: {
 	name: string;
@@ -27,26 +27,34 @@ export default function Header({
 			</div> */}
 			{/* <h1 className='sr-only'>Supabase and Next.js Starter Template</h1> */}
 
-			<div className='text-2xl  lg:text-3xl !leading-tight mx-auto max-w-4xl text-center mb-16'>
-				<div className='font-serif flex flex-col -skew-y-3 drop-shadow-xl my-10  text-[4.25rem] sm:text-[5.5rem] tracking-[-0.03em] leading-[0.88] font-bold'>
-					<span className='underline decoration-green-500/50'>{name}</span>
+			<section className='text-gray-600 body-font flex flex-col justify-center items-center align-middle'>
+				<div className='container mx-auto flex px-5 pt-10 md:pt-24 items-center justify-center flex-col'>
+				
+					<div className='text-center w-full flex flex-col justify-center align-middle items-center'>
+						<div className='font-serif flex text-white flex-col -skew-y-3 drop-shadow-xl mt-10  text-[4.25rem] sm:text-[5.5rem] tracking-[-0.03em] leading-[0.88] font-bold'>
+							<span className='underline decoration-green-500/50 '>{name}</span>
+						</div>
+
+					</div>
 				</div>
-			</div>
-			
+			</section>
+
+		
+
 			<div className='w-full p-[1px] bg-gradient-to-r from-transparent via-foreground/10 to-transparent my-8 ' />
 
-			{ total >= 0 && <div className=' flex flex-row gap-x-8 sm:gap-x-8 md:gap-x-20 justify-center items-center mx-auto'>
-				<div className=' flex flex-col justify-center align-middle text-center'>
-					<span className='font-serif flex flex-col mt-10 mb-6  text-[1.25rem] sm:text-[2rem] tracking-[-0.03em] leading-[0.88] font-bold'>{sectiontext}</span>
-					<span className='text-[2.25rem] sm:text-[3rem] tracking-[-0.03em] font-bold border-2 items-center transition ease-in duration-200 py-2 px-1 rounded-full opacity translate-y-[10px] group font-mono uppercase leading-[1.2] '>
-		
-						{total}
-					</span>
+			{total >= 0 && (
+				<div className=' flex flex-row gap-x-8 sm:gap-x-8 md:gap-x-20 justify-center items-center mx-auto'>
+					<div className=' flex flex-col justify-center align-middle text-center'>
+						<span className='font-serif flex flex-col mt-10 mb-6  text-[1.25rem] sm:text-[2rem] tracking-[-0.03em] leading-[0.88] font-bold'>
+							{sectiontext}
+						</span>
+						<span className='text-[2.25rem] sm:text-[3rem] tracking-[-0.03em] font-bold border-2 items-center transition ease-in duration-200 py-2 px-1 rounded-full opacity translate-y-[10px] group font-mono uppercase leading-[1.2] '>
+							{total}
+						</span>
+					</div>
 				</div>
-
-				
-			 
-			</div>}
+			)}
 		</div>
 	);
 }
